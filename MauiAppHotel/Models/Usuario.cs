b4a27ee9cbef1d32cf74f58d7be3a6b9a8676666
unknown <vhugodossantos33@gmail.com>
2026-06-15 19:07:27 -0300

    :sparkles: feat: versão final.

---
+++ MauiAppHotel/Models/Usuario.cs
@@ -0,0 +1,19 @@
+﻿namespace MauiAppHotel.Models
+{
+    public class Usuario
+    {
+        string _nome = "";
+
+        public string Nome { 
+            get => _nome; 
+            set
+            {
+                if (value == null)
+                    throw new Exception("Informe seu nome.");
+            }
+        }
+                
+        public string Email { get; set; }
+        public string Senha { get; set; }
+    }
+}
---
+++ MauiAppHotel/Views/HospedagemContratada.xaml.cs
@@ -0,0 +1,32 @@
+namespace MauiAppHotel.Views;
+
+public partial class HospedagemContratada : ContentPage
+{
+    bool usuario_logado = false;
+
+	public HospedagemContratada()
+	{
+		InitializeComponent();
+	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    protected override async void OnAppearing()
+    {
+        string? email_usuario = await SecureStorage.Default.GetAsync("nome_usuario");
+
+        if (email_usuario != null)
+            usuario_logado = true;
+    }
+
+    private async void Button_Clicked_Avancar(object sender, EventArgs e)
+    {
+        if (!usuario_logado)
+            await Navigation.PushAsync(new Views.Login());
+        else
+            await DisplayAlertAsync("Oba!", "Você já está logado! Hora de pagar!", "OK");
+    }
+}
